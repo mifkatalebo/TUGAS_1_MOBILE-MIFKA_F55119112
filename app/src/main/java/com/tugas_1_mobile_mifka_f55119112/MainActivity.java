5b938a84d63906a6096e8c8519eb4717a7839b29
mifkatalebo <mifkatalebo@gmail.com>
2022-03-05 21:44:19 +0800

    Initial commit

---
+++ app/src/main/java/com/tugas_1_mobile_mifka_f55119112/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tugas_1_mobile_mifka_f55119112;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
